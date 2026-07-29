345686922ba3f2675deec4b2b27e2b1b8826797c
maederastgarfr <maedehrastegarfar@gmail.com>
2026-07-29 16:57:31 +0330

    feat: add SelectCategoryViewComponent implementation

---
+++ EShop.Web/Areas/Admin/ViewComponents/SelectCategoryViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.Application.Services.Interfaces;
+using EShop.Data.DTOs.ProductCategoryDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Areas.Admin.ViewComponents
+{
+    public class SelectCategoryViewComponent : Controller
+    {
+        private readonly IProductService _productService;
+        public SelectCategoryViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsunc(long? parentId, int level, List<long>? selectedCategoriesIds)
+        {
+            var categories = await _productService.GetAllCategories(parentId);
+            var model = new TreeViewCategoriesDto
+            {
+                Level = level,
+                ProductCategories = categories,
+                SelectedCategoriesIds=selectedCategoriesIds
+            };
+            return View("SelectCategories", model);
+        }
+    }
+}
