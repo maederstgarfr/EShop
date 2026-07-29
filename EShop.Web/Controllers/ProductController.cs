43b925200c7e6723aebb3d48d92de7e3dfb4b626
maederastgarfr <maedehrastegarfar@gmail.com>
2026-07-29 22:02:17 +0330

    feat: add ProductController and implement product filter and detail

---
+++ EShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.Application.Services.Interfaces;
+using EShop.Data.DTOs.ProductDTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Controllers
+{
+    public class ProductController : SiteBaseController
+    {
+        #region CTOR
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        #endregion
+
+        #region Filter Products
+        [HttpGet("product-list")]
+        public async Task<IActionResult> FilterProducts(FilterProductDto filter)
+        {
+            var model = await _productService.FilterProduct(filter);
+            return View(model);
+        }
+        #endregion
+
+        #region  Product Detail
+        [HttpGet("product-list")]
+        public async Task<IActionResult> ProductDetail(long productId)
+        {
+            var model = await _productService.ProductDetail(productId);
+            return View(model);
+        }
+        #endregion
+    }
+}
