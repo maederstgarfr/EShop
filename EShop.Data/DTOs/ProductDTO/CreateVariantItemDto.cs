b36be3825af34d924fef23763b91c622dfe7acd3
maederastgarfr <maedehrastegarfar@gmail.com>
2026-07-29 18:32:30 +0330

    refactor: rename to CreateVariantItemDto

---
+++ EShop.Data/DTOs/ProductDTO/CreateVariantItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Data.DTOs.ProductDTO
+{
+    public class CreateVariantItemDto
+    {
+        public long ColorId { get; set; }
+        //public long productId { get; set; }
+        public string? Price { get; set; }
+        public int StockCount { get; set; }
+
+    }
+}
