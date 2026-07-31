af16a24aaf4f945d9311b31b1cb582c3fba3ac39
maederastgarfr <maedehrastegarfar@gmail.com>
2026-07-31 18:54:53 +0330

    feat: extend IOrderService interface

--- EShop.Application/Services/Interfaces/IOrderService.cs
+++ EShop.Application/Services/Interfaces/IOrderService.cs
@@ -11,8 +11,11 @@ namespace EShop.Application.Services.Interfaces
         Task<FilterOrderDto> FilterOrders(FilterOrderDto filter);
         Task<Order> GetUserOpenOrder(long userId);
         Task<ProcessOrderDto> GetProcessOrder(long orderId);
+        Task<long> AddOrderForUser(long userId);
         Task ProcessOrder(ProcessOrderDto dto);
         Task AddProductToOrder(SubmitOrderDto dto);
+        Task ChangeOrderDetailCount(long orderDetailId, int count);
+        Task RemoveOrderDetail(long orderDetailId);
         Task PayOrderPrice(long invoiceId);
         Task DeleteOrder(long orderId);
 
