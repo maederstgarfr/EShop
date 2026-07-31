582c35364dc8f2304c5c58e798ec9e6df1d842e2
maederastgarfr <maedehrastegarfar@gmail.com>
2026-07-31 17:05:14 +0330

    feat: Add OrderService

---
+++ EShop.Application/Services/Implementations/OrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EShop.Application.Services.Interfaces;
+
+namespace EShop.Application.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
