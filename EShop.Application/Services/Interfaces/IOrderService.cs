fd9496508fcca2b295a4bac5ecbd714bfc4ef6ef
maederastgarfr <maedehrastegarfar@gmail.com>
2026-07-31 17:08:00 +0330

    feat:Define IOrderService Contract

---
+++ EShop.Application/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using EShop.Data.DTOs.OrderDto;
+using EShop.Data.Entities.OrderEntities;
+
+namespace EShop.Application.Services.Interfaces
+{
+    public interface IOrderService: IAsyncDisposable
+    {
+        Task<OrderDetailDto> OrderDetail(long orderId);
+        Task<FilterOrderDto> FilterOrders(FilterOrderDto filter);
+        Task<Order> GetUserOpenOrder(long userId);
+        Task<ProcessOrderDto> GetProcessOrder(long orderId);
+        Task ProcessOrder(ProcessOrderDto dto);
+        Task AddProductToOrder(SubmitOrderDto dto);
+        Task PayOrderPrice(long invoiceId);
+        Task DeleteOrder(long orderId);
+
+    }
+}
