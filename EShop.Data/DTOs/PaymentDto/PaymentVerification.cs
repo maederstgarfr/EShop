aec7bb030d4818195c75c7e99ec33f961f4b8d30
maederastgarfr <maedehrastegarfar@gmail.com>
2026-08-03 01:23:01 +0330

    PaymentVerificationResultData dto

---
+++ EShop.Data/DTOs/PaymentDto/PaymentVerification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Data.DTOs.PaymentDto
+{
+    class PaymentVerification
+    {
+    }
+}
