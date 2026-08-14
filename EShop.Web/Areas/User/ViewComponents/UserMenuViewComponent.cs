fce97daf14d889bb71030469e803605ec81b6052
maederastgarfr <maedehrastegarfar@gmail.com>
2026-08-14 15:17:46 +0330

    feat: add UserMenuViewComponent in user Area and SiteHeaderViewComponent

---
+++ EShop.Web/Areas/User/ViewComponents/UserMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.Application.Services.Interfaces;
+using EShop.Web.UserExtentions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Areas.User.ViewComponents
+{
+    public class UserMenuViewComponent: ViewComponent
+    {
+        private readonly IUserService _userService;
+        public UserMenuViewComponent(IUserService userService)
+        {
+            _userService = userService;
+        }
+         public async Task<IActionResult> InvokeAsync()
+        {
+            ViewData["User"] = await _userService.GetUserbyId(User.GetUserId());
+            return View("UserMenu");
+        }
+    }
+}
--- EShop.Web/ViewComponents/SiteHeaderViewComponent.cs
+++ EShop.Web/ViewComponents/SiteHeaderViewComponent.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EShop.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.Web.ViewComponents
 {
     public class SiteHeaderViewComponent : ViewComponent
     {
+        private readonly IProductService _productService;
+        public SiteHeaderViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            ViewData["categories"] = await _productService.GetAllActiveCategories();
             return View("SiteHeader");
         }
     }
