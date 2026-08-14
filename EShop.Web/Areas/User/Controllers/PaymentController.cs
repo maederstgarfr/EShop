2bb4aa301614234457d300469bc25c50feb0db30
maederastgarfr <maedehrastegarfar@gmail.com>
2026-08-14 17:42:56 +0330

    feat: complete PayOrder

--- EShop.Web/Areas/User/Controllers/PaymentController.cs
+++ EShop.Web/Areas/User/Controllers/PaymentController.cs
@@ -1,7 +1,11 @@
 ﻿using System.Threading.Tasks;
+using Eshop.Data.DTOs.PaymentDto;
 using EShop.Application.Services.Interfaces;
+using EShop.Web.Areas.User.Services;
 using EShop.Web.UserExtentions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace EShop.Web.Areas.User.Controllers
 {
@@ -10,12 +14,15 @@ namespace EShop.Web.Areas.User.Controllers
         #region CTOR
         private readonly IUserService _userService;
         private readonly IOrderService _orderService;
+        private readonly IConfiguration _configuration;
+        private readonly IPaymentService _paymentService;
 
-
-        public PaymentController(IUserService userService, IOrderService orderService,)
+        public PaymentController(IUserService userService, IOrderService orderService, IConfiguration configuration, IPaymentService paymentService)
         {
             _userService = userService;
             _orderService = orderService;
+            _configuration = configuration;
+            _paymentService = paymentService;
         }
 
         #endregion
@@ -34,15 +41,52 @@ namespace EShop.Web.Areas.User.Controllers
             return View();
             #endregion
 
+            var order = await _orderService.UserOpenOrderDetail(User.GetUserId());
+            if (order == null)
+            {
+                TempData[ErrorMessage] = "سبد خرید شما خالی است";
+                return RedirectToAction("Cart", "Order");
+            }
+
+            if (order.TotalCartPrice() * 10 > 1000000000)
+            {
+                TempData[ErrorMessage] = "سفارشی که بیش از 100 میلیون تومان باشد قابل پرداخت نیست . لطغا از تعداد سفارش خود بکاهید.";
+                return RedirectToAction("Cart", "Order");
+            }
+
+            var updatedPrice = await _orderService.UpdateOrderDetailPrices(order.Order.Id);
+
+            var paymentRequest = new PaymentRequest
+            {
+                merchant_id = IConfiguration.GetValue<string>("NovinoPayment:MerchantId") ?? "",
+                amount = updatedPrice * 10,
+                invoice_id = order.Order.Id.ToString(),
+                description = "پرداخت سفارش از وبسایت Eshop",
+                callback_url = _configuration.GetValue<string>("NovinoPayment:PaymentCallbackUrl") ?? "",
+            };
+
+            var requestPaymentResult = await _paymentService.CreatePayment(paymentRequest);
+
+            if (requestPaymentResult == null) return NotFound();
+
+            switch (requestPaymentResult.status)
+            {
+                case "100":
+                    return Redirect(requestPaymentResult.data.payment_url);
+            }
+
+            return NotFound();
 
         }
         #endregion
 
         #region Call back
+        [AllowAnonymous]
         [HttpGet("payment-result")]
-        public async Task<IActionResult> PaymentResult()
+        public async Task<IActionResult> PaymentResult(string paymentStatus, string authority, string invoiceId)
         {
-            return View();
+            var order = _orderService.GetOrderById(long.Parse(invoiceId));
+            var OrderPrice = _orderService.GetOrderTotalPrice(long.Parse(invoiceId));
         }
         #endregion
     }
