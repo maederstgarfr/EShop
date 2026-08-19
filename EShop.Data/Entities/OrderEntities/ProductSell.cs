985e2d48e13c7057b525fce841b0234b6980fb09
maederastgarfr <maedehrastegarfar@gmail.com>
2026-08-19 18:37:26 +0330

    Feat: Add ProductSell entity

---
+++ EShop.Data/Entities/OrderEntities/ProductSell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EShop.Data.Entities.Common;
+
+namespace EShop.Data.Entities.OrderEntities
+{
+    public class ProductSell : BaseEntitiy
+    {
+        public long ProductId { get; set; }
+        public int SellCount { get; set; }
+        public long OrderId { get; set; }
+        public int ProductPrice { get; set; }
+        public DateTime SellDate { get; set; }
+    }
+}
+}
