065a577d7d0b058307cb793bb8d2818ff489fa07
maederastgarfr <maedehrastegarfar@gmail.com>
2026-08-19 18:52:07 +0330

    feat:Add Common Service

---
+++ EShop.Application/Services/Implementations/CommonService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EShop.Application.Services.Interfaces;
+using EShop.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Application.Services.Implementations
+{
+    public class CommonService : ICommonService
+    {
+        public Task<FileInfo> GetSiteInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
